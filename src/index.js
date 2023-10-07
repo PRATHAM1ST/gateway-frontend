878cfb8eb70438999bb07f8dc1d01919a486fba3
Pratham <chudasamapratham142@gmail.com>
2023-10-08 00:07:20 +0530

    react router added

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import NewTeams from "./components/NewTeams";
+import OnboardTeamMember from "./components/OnboardTeamMember";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <App/>,
+	},
+  {
+    path: "/new-teams",
+    element: <NewTeams />,
+  },
+  {
+    path: "/onboard-team-member",
+    element: <OnboardTeamMember />,
+  }
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
