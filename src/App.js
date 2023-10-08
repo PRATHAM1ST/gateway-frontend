08643e629255d04dab0cf719e8cbb634c3a00534
vanshita Roopchandani <81114486+vanshitaroop@users.noreply.github.com>
2023-10-08 06:54:03 +0530

    calender added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import './App.css';
-import HomePage from './components/HomePage';
-import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import "./App.css";
+import HomePage from "./components/HomePage";
+import "mdb-react-ui-kit/dist/css/mdb.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import SigninState from "./context/signin/SigninState";
+import CalenderUI from "./components/CalenderUI";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from "react-router-dom";
 function App() {
   return (
-  <>
-  <HomePage/>
-  </>
+    <>
+    <Router>
+      <SigninState>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/CalenderUI" element={<CalenderUI />} />
+        </Routes>
+      </SigninState>
+    </Router>
+    </>
   );
 }
 
