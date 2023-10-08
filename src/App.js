dea898ea135e04a630e28b17288680b7bf8008c1
Pratham <chudasamapratham142@gmail.com>
2023-10-08 07:04:07 +0530

    merge conflict fix

--- src/App.js
+++ src/App.js
@@ -2,13 +2,26 @@ import "./App.css";
 import HomePage from "./components/HomePage";
 import "mdb-react-ui-kit/dist/css/mdb.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
-
+import SigninState from "./context/signin/SigninState";
+import CalenderUI from "./components/CalenderUI";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from "react-router-dom";
 function App() {
-	return (
-		<>
-			<HomePage />
-		</>
-	);
+  return (
+    <>
+    <Router>
+      <SigninState>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/CalenderUI" element={<CalenderUI />} />
+        </Routes>
+      </SigninState>
+    </Router>
+    </>
+  );
 }
 
 export default App;
